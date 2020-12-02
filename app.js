60f8c8f85ab3faa914ef388e895189633b63af69
Amr Labib <amr.h.labib@gmail.com>
2020-12-02 17:04:11 +0200

    move from gulp to webpack

--- app.js
+++ app.js
@@ -4,45 +4,31 @@ var session = require('express-session');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-
+// express middlewares
 app.use(function(req, res, next) {
-
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
     next();
 });
-
-//express middlewares
 app.use('/resources', express.static(__dirname + '/dist/resources'));
 app.use('/favicon', express.static(__dirname + '/dist/favicon'));
-
-
-
+app.use('/fonts', express.static(__dirname + '/dist/fonts'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
 
-
-
+// routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/dist/" + "index.html");
 });
 
+app.get('/index.js', (req, res) => {
+    res.sendFile(__dirname + "/dist/" + "index.js");
+});
 
-
-// Start Node server on Port 3000
 app.listen( process.env.PORT || 5000, () => {
     console.log('listening on 5000');
 });
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        use: {
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+            },
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: {
+            loader: 'svg-url-loader',
+            options: {
+                encoding: 'base64'
+            }
+        }
+      }
+    ],
+  },
+  plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './src/index.html', to: './' },
+          { from: './src/images/*', to: './' },
+        ]}
+       ),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    disableHostCheck: true
+  }
+};
